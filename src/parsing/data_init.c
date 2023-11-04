aace7b933decb3236c978ce009535fca0f48dc58
AlphomArt <menelik.wubneh@gmail.com>
2023-11-04 14:43:09 +0100

    documentation for data_init.c

--- src/parsing/data_init.c
+++ src/parsing/data_init.c
@@ -6,7 +6,7 @@
 /*   By: mwubneh <mwubneh@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/21 14:56:38 by mwubneh           #+#    #+#             */
-/*   Updated: 2023/11/01 08:29:41 by mwubneh          ###   ########.fr       */
+/*   Updated: 2023/11/04 14:42:42 by mwubneh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 
 static void	element_init(void *element[6], t_data *data);
 
+/**
+ * @brief The function stores the number of lines
+ * 		read plus 1 at the address `file_size` and then terminates.
+ * @param *data address of parameter whose elements need to be initialized
+ * @return The program exits after the elements of the structure
+ * 			located at the address passed as a parameter have been initialized.
+ */
 void	data_init(t_data *data)
 {
 	int	i;
@@ -31,6 +38,12 @@ void	data_init(t_data *data)
 	element_init(data->element, data);
 }
 
+/**
+ * @brief Initialize an array of addresses (*element[6])
+ *			to make the assignment of data elements more flexible.
+ * @param *data structures whose elements need to be
+ *				assigned to the array of addresses
+ */
 static void	element_init(void *element[6], t_data *data)
 {
 	(element)[0] = &data->no;
